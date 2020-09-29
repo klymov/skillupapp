5ecc7573b8e31527cce9aedd6323f6c145b1d3fb
klymov <gtp.klimov@gmail.com>
2020-09-30 00:23:00 +0300

    add Order

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'order/index'
+  # get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :orders
+
+  #root 'order#index'
 end
